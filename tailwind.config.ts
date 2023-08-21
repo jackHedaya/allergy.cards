a73326b106b09753a3976100f04c35551dd2d578
Jack Hedaya <jackehedaya@gmail.com>
2023-08-21 16:35:49 -0400

    changed analytics mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/shared/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/shared/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        acpink: "#FF6DB3",
-        acblue: "#19213D",
+        acpink: '#FF6DB3',
+        acblue: '#19213D',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
